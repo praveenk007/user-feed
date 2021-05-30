641f0c0939e75944d0b2236013361bdcef3060ea
Praveen Kamath <kamathp@synopsys.com>
2021-05-30 15:37:38 +0530

    first commit

---
+++ src/main/java/com/crowdfire/userfeed/enums/PostType.java
@@ -0,0 +1,12 @@
+package com.crowdfire.userfeed.enums;
+
+/**
+ * @author kamathp
+ * @version 0.0.1
+ */
+public enum PostType {
+
+    COMMENT,
+
+    STATUS
+}
---
+++ src/test/java/com/crowdfire/userfeed/UserFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdfire.userfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
